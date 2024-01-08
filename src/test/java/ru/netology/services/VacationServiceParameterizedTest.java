f02016074759be822341877d2bbc3fd03e6b9a44
Леонтьев Дмитрий QA-71 <leontyev.goods@gmail.com>
2024-01-09 00:15:18 +0300

    Second commit

--- src/test/java/ru/netology/services/VacationServiceParameterizedTest.java
+++ src/test/java/ru/netology/services/VacationServiceParameterizedTest.java
@@ -2,15 +2,17 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.services.VacationService;
 
 public class VacationServiceParameterizedTest {
     @ParameterizedTest
-    @CsvSource({
-            "100000,60000,150000,2",
-            "10000,3000,20000,3"
-    })
+    // @CsvSource({
+//            "100000,60000,150000,2",
+    //           "10000,3000,20000,3"
+    //  })
+    @CsvFileSource(files = "src/test/resources/Vacation.csv")
 
 
     public void shouldCalcVacationСonditions(int income, int expenses, int threshold, int expected) {
