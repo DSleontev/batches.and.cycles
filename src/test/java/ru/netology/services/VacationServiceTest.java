be50e74ebca379024799d7245a4f183e96496c58
Леонтьев Дмитрий QA-71 <leontyev.goods@gmail.com>
2024-01-09 15:30:04 +0300

    Reformat code again commit

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -1,3 +1,5 @@
+package ru.netology.services;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.services.VacationService;
@@ -6,19 +8,19 @@ import ru.netology.services.VacationService;
 public class VacationServiceTest {
 
     @Test
-    public void shouldCalcVacationСonditionsOne () {
+    public void shouldCalcVacationConditionsOne() {
         VacationService services = new VacationService();
         int expected = 2;
-        int actual = services.calculate(100000,60000,150000);
+        int actual = services.calculate(100000, 60000, 150000);
 
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldCalcVacationСonditionsTwo () {
+    public void shouldCalcVacationConditionsTwo() {
         VacationService services = new VacationService();
         int expected = 3;
-        int actual = services.calculate(10000,3000,20000);
+        int actual = services.calculate(10000, 3000, 20000);
 
         Assertions.assertEquals(expected, actual);
     }
